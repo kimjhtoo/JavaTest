d762cb67474fe0e700277700d425865997dc6171
kimjhtoo <kimjhtoo96@gmail.com>
2022-04-13 10:57:32 +0900

    22.04.13 '프로그래밍 실습' 커밋

---
+++ src/main/java/Ex07_03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex07_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a ,b;
+
+        while(true) {
+            System.out.printf("더할 첫번째 수 입력 :");
+            a = sc.nextInt();
+            System.out.printf("더할 두번재 수 입력 :");
+            b=  sc.nextInt();
+
+            System.out.printf("%d + %d = %d \n" , a , b , a+b);
+
+        }
+
+    }
+}
