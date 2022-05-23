7f7e6bfeefcd614163fb1c7b4da14257fdca5d9c
kimjhtoo <kimjhtoo96@gmail.com>
2022-05-24 08:24:52 +0900

    22.05.24 "Commit"

---
+++ src/main/java/EndStartDemo.java
@@ -0,0 +1,9 @@
+public class EndStartDemo {
+    public static void main(String[] args) {
+
+        String al = "Hello Java Devlopers";
+
+        System.out.println(al.startsWith("He"));
+        System.out.println(al.endsWith("pers"));
+    }
+}
