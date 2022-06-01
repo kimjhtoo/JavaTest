e54aeeb2bd0bdce2f7e7e1f4690620ef67158739
kimjhtoo <kimjhtoo96@gmail.com>
2022-06-02 08:44:41 +0900

    22.06.02 "Commit"

--- src/main/java/Dimaond5.java
+++ src/main/java/Dimaond5.java
@@ -1,23 +1,23 @@
 public class Dimaond5 {
     public static void main(String[] args) {
         //---------------상단 삼각형 출력-------------------//
-        for (int i = 1; i <= 3; i++) { // 위에 줄 총 3줄
-            for (int j = 1; j <= 3 - i; j++) { // 공백 2개 > 1개
-                System.out.printf(" ");
+        for (int i = 1; i <= 4; i++) { // 위에 줄 총 3줄
+            for (int j = 1; j <= 4 - i; j++) { // 공백 2개 > 1개
+                System.out.print(" ");
             }
             for (int j = 1; j <= i * 2 - 1; j++) { //별 1개 > 3개 > 5개
-                System.out.printf("*");
+                System.out.print("*");
             }
             System.out.println(); //상단 삼각형 개행완료
         }
         //--------------하단 삼각형 출력--------------------//
 
-        for (int i = 2; i > 0; i--) { // 밑에 줄 총 2줄
-            for (int j = 2; j >= i; j--) { // 공백 1개 < 2개
-                System.out.printf(" ");
+        for (int i = 3; i > 0; i--) { // 밑에 줄 총 2줄
+            for (int j = 3; j >= i; j--) { // 공백 1개 < 2개
+                System.out.print(" ");
             }
             for (int j = 1; j <= i * 2 - 1; j++) { // 별 3개 > 1개
-                System.out.printf("*");
+                System.out.print("*");
             }
             System.out.println(); // 개행
         }
